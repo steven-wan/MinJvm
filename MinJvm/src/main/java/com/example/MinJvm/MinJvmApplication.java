ca2f2c8c1f7aaf6544038ec5354c528fa727b74f
steven.wan <951520698@qq.com>
2020-04-23 16:02:58 +0800

    dd

---
+++ MinJvm/src/main/java/com/example/MinJvm/MinJvmApplication.java
@@ -0,0 +1,13 @@
+package com.example.MinJvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinJvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinJvmApplication.class, args);
+	}
+
+}
